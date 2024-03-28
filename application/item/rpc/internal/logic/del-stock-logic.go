375a4ce000ce5a802bb7e6d043e59b1b8ad4c11a
pwh <3025550698@qq.com>
2024-03-28 17:00:17 +0800

    fix:优化item rpc服务扣减库存

--- application/item/rpc/internal/logic/del-stock-logic.go
+++ application/item/rpc/internal/logic/del-stock-logic.go
@@ -1,7 +1,6 @@
 package logic
 
 import (
-	"bufio"
 	"context"
 	"database/sql"
 	"github.com/dtm-labs/client/dtmcli"
@@ -15,8 +14,6 @@ import (
 	"hmall/application/item/rpc/pb"
 	"hmall/application/item/rpc/types"
 	"hmall/pkg/util"
-	"io"
-	"os"
 	"strconv"
 	"sync"
 )
@@ -35,11 +32,22 @@ func NewDelStockLogic(ctx context.Context, svcCtx *svc.ServiceContext) *DelStock
 	}
 }
 
+var decut_lua = `
+	local acquired = redis.call("exist",KEYS[2])
+	if acquired == 1 then
+		local stock = redis.call("HGET",KEYS[2],KEYS[3])
+		redis.call("HSET",KEYS[2],KEYS[3],stock - ARGV[2])
+		redis.call("EXPIRE",KEYS[2],ARGV[3])
+	end
+`
+
 func (l *DelStockLogic) DelStock(in *pb.DelStockReq) (*pb.DelStockResp, error) {
 	barrier, err := dtmgrpc.BarrierFromGrpc(l.ctx)
 	db, err := sqlx.NewMysql(l.svcCtx.Config.DB.DataSource).RawDB()
 
 	if err = barrier.CallWithDB(db, func(tx *sql.Tx) error {
+		wg := &sync.WaitGroup{}
+		var res error = nil
 		for _, val := range in.Detail {
 			item, err := l.svcCtx.ItemModel.DecutStock(l.ctx, val.ItemId, val.Num)
 			if err != nil {
@@ -48,11 +56,10 @@ func (l *DelStockLogic) DelStock(in *pb.DelStockReq) (*pb.DelStockResp, error) {
 			}
 
 			//库存不足回滚
-			if item.Stock < 0 {
-				return status.Error(codes.Aborted, dtmcli.ResultFailure)
+			if res == nil && item.Stock < 0 {
+				res = status.Error(codes.Aborted, dtmcli.ResultFailure)
 			}
 
-			wg := &sync.WaitGroup{}
 			wg.Add(2)
 			//更新缓存
 			threading.GoSafe(func() {
@@ -73,9 +80,9 @@ func (l *DelStockLogic) DelStock(in *pb.DelStockReq) (*pb.DelStockResp, error) {
 					panic(err)
 				}
 			})
-			wg.Wait()
 		}
-		return nil
+		wg.Wait()
+		return res
 	}); err != nil {
 		return nil, err
 	}
@@ -86,45 +93,16 @@ func (l *DelStockLogic) DelStock(in *pb.DelStockReq) (*pb.DelStockResp, error) {
 func (l *DelStockLogic) UpdateCache(itemId string, stock int64) error {
 	lockKey := util.CacheKey(types.CacheStockLock, itemId)
 	key := util.CacheKey(types.CacheItemKey, itemId)
-	exists, _ := l.svcCtx.BizRedis.Exists(key)
-	//缓存存在，更新缓存
-	if exists {
-		//读取lua脚本
-		luaScript, err := ReadLua()
-		if err != nil {
-			return err
-		}
-		// 执行Lua脚本
-		_, err = l.svcCtx.BizRedis.Eval(luaScript, []string{
-			lockKey,
-			key,
-			types.CacheItemStock,
-		}, itemId, strconv.FormatInt(stock, 10), types.CacheItemTime)
-		if err != nil && err.Error() != "redis: nil" {
-			logx.Errorf("BizRedis.Eval, error: %v", err)
-			return err
-		}
+
+	// 执行Lua脚本
+	_, err := l.svcCtx.BizRedis.Eval(decut_lua, []string{
+		lockKey,
+		key,
+		types.CacheItemStock,
+	}, itemId, strconv.FormatInt(stock, 10), types.CacheItemTime)
+	if err != nil && err.Error() != "redis: nil" {
+		logx.Errorf("BizRedis.Eval, error: %v", err)
+		return err
 	}
 	return nil
-}
-
-func ReadLua() (string, error) {
-	//打开脚本
-	file, err := os.Open(types.Luapath)
-	if err != nil {
-		logx.Errorf("os.Open: %v,error: %v", types.Luapath, err)
-		return "", err
-	}
-	reader := bufio.NewReader(file)
-	var luaScript string
-	//逐行读取
-	for {
-		line, err := reader.ReadString('\n')
-		luaScript += line
-		if err == io.EOF {
-			break
-		}
-	}
-
-	return luaScript, nil
 }
--- application/item/rpc/internal/logic/del-stock-roll-back-logic.go
+++ application/item/rpc/internal/logic/del-stock-roll-back-logic.go
@@ -1,7 +1,6 @@
 package logic
 
 import (
-	"bufio"
 	"context"
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/threading"
@@ -9,8 +8,6 @@ import (
 	"hmall/application/item/rpc/pb"
 	"hmall/application/item/rpc/types"
 	"hmall/pkg/util"
-	"io"
-	"os"
 	"strconv"
 	"sync"
 )
@@ -29,23 +26,31 @@ func NewDelStockRollBackLogic(ctx context.Context, svcCtx *svc.ServiceContext) *
 	}
 }
 
+var decut_rollback_lua = `
+	local acquired = redis.call("exist",KEYS[2])
+	if acquired == 1 then
+		local stock = redis.call("HGET",KEYS[2],KEYS[3])
+		redis.call("HSET",KEYS[2],KEYS[3],stock + ARGV[2])
+		redis.call("EXPIRE",KEYS[2],ARGV[3])
+	end
+`
+
 func (l *DelStockRollBackLogic) DelStockRollBack(in *pb.DelStockReq) (*pb.DelStockResp, error) {
+	wg := &sync.WaitGroup{}
 	for _, val := range in.Detail {
 		item, err := l.svcCtx.ItemModel.AddStock(l.ctx, val.ItemId, val.Num)
 		if err != nil {
 			logx.Errorf("ItemModel.DecutStock: %v, error: %v", val, err)
 			return nil, err
 		}
-
-		wg := &sync.WaitGroup{}
 		wg.Add(2)
 		//写缓存
 		threading.GoSafe(func() {
 			defer wg.Done()
 			key := util.CacheKey(types.CacheItemKey, strconv.FormatInt(item.Id, 10))
-			err = l.svcCtx.BizRedis.Set(key, strconv.Itoa(int(item.Stock)))
+			err = l.UpdateCacheRollBack(val.ItemId, val.Num)
 			if err != nil {
-				logx.Errorf("BizRedis.Set: %v, error: %v", key, err)
+				logx.Errorf("UpdateCacheRollBack: %v, error: %v", key, err)
 			}
 		})
 
@@ -58,53 +63,24 @@ func (l *DelStockRollBackLogic) DelStockRollBack(in *pb.DelStockReq) (*pb.DelSto
 				logx.Errorf("pusherSearch.PusherSearch: %v, error: %v", *item, err)
 			}
 		})
-		wg.Wait()
 	}
+	wg.Wait()
 	return &pb.DelStockResp{}, nil
 }
 
 func (l *DelStockRollBackLogic) UpdateCacheRollBack(itemId string, stock int64) error {
 	lockKey := util.CacheKey(types.CacheStockLock, itemId)
 	key := util.CacheKey(types.CacheItemKey, itemId)
-	exists, _ := l.svcCtx.BizRedis.Exists(key)
-	//缓存存在，更新缓存
-	if exists {
-		//读取lua脚本
-		luaScript, err := ReadLuaRollBack()
-		if err != nil {
-			return err
-		}
-		// 执行Lua脚本
-		_, err = l.svcCtx.BizRedis.Eval(luaScript, []string{
-			lockKey,
-			key,
-			types.CacheItemStock,
-		}, itemId, strconv.FormatInt(stock, 10), types.CacheItemTime)
-		if err != nil && err.Error() != "redis: nil" {
-			logx.Errorf("BizRedis.Eval, error: %v", err)
-			return err
-		}
+
+	_, err := l.svcCtx.BizRedis.Eval(decut_rollback_lua, []string{
+		lockKey,
+		key,
+		types.CacheItemStock,
+	}, itemId, strconv.FormatInt(stock, 10), types.CacheItemTime)
+	if err != nil && err.Error() != "redis: nil" {
+		logx.Errorf("BizRedis.Eval, error: %v", err)
+		return err
 	}
+
 	return nil
-}
-
-func ReadLuaRollBack() (string, error) {
-	//打开脚本
-	file, err := os.Open(types.LuapathRollBack)
-	if err != nil {
-		logx.Errorf("os.Open: %v,error: %v", types.Luapath, err)
-		return "", err
-	}
-	reader := bufio.NewReader(file)
-	var luaScript string
-	//逐行读取
-	for {
-		line, err := reader.ReadString('\n')
-		luaScript += line
-		if err == io.EOF {
-			break
-		}
-	}
-
-	return luaScript, nil
 }
